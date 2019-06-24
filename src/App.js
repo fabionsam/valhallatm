041cb8dea84e1a9546ede470f0c0bb89ce350210
Fábio Custódio <fabionsam@gmail.com>
2019-06-24 14:31:24 -0300

    Ajustes
    ALterado local onde carregaria os js da página.

--- src/App.js
+++ src/App.js
@@ -5,28 +5,6 @@ import "./assets/css/theme.css";
 import "./assets/css/lightbox/lightbox.css";
 
 class App extends React.Component {
-  componentDidMount () {
-    var script = document.createElement("script");
-    script.src = process.env.PUBLIC_URL + "/js/jquery-3.4.1.min.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    script = document.createElement("script");
-    script.src = process.env.PUBLIC_URL + "/js/main.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    script = document.createElement("script");
-    script.src = process.env.PUBLIC_URL + "/js/theme.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    script = document.createElement("script");
-    script.src = process.env.PUBLIC_URL + "/js/lightbox/lightbox.min.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }
-
   render(){ 
     return(
       <div className="App">    
